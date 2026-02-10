627c907140f938068f50e417ca9be2970eab5cda
djharryjr-cmyk <djharryjr@gmail.com>
2026-02-10 10:27:23 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,20 @@
-# Mini Project — Decision Quiz
-#Student Name: 
-# Topic: If / Elif / Else
-# Goal: Create a quiz that gives different feedback based on the user's answer
-
-print("Welcome to the Decision Quiz!")
-print("-----------------------------")
-
-# TODO 1: Print your quiz question
-# Example: print("What is 2 + 2?")
-print("ENTER YOUR QUIZ QUESTION HERE")
-
-# TODO 2: Ask the user for their answer
-# Convert to int() only if you are comparing numbers
-answer = input("Enter your answer: ")
-
-# TODO 3: Create at least THREE possible outcomes using:
-# - if
-# - elif
-# - else
-#
-# Each outcome should print a different message
-# Example ideas:
-# - Correct answer
-# - Close answer
-# - Incorrect answer
-
-# Write your decision logic below
+# David Harry
 
 
+print("Welcome to the Elite Quiz!")
 
-# TODO 4: Print a closing message
-print("Thanks for taking the quiz!")
+print("What is your time on the Elite speed trial?")
+
+Speed =int(input("What is your Elite speed trial time: "))
+
+if Speed >= 50:
+  print("you are running at or better than the average olympic speed!")
+
+elif Speed >= 45:
+  print ("you are running at or better than the average olympic speed!")
+
+else:
+  print ("you are running less than average olympic and college speed!")
+
+print("Now go do work!")
+
